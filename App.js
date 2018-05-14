e4aa7d281fc9ea48e7af236360e50408b92275ab
Brane Opacic <brane.opacic@brightmarbles.io>
2018-05-14 16:11:17 +0100

    Slowly ending office work for today

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { Navigation } from 'react-native-navigation';
 import { Provider } from 'react-redux';
 
+
 import AuthScreen from './src/screens/Auth/Auth';
 import SharePlaceScreen from './src/screens/SharePlace/SharePlace';
 import FindPlaceScreen from './src/screens/FindPlace/FindPlace';
