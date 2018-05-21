70db36d3c01cb46491471982780a3849c32276d7
Brane Opacic <brane.opacic@brightmarbles.io>
2018-05-21 16:13:58 +0100

    Removed functions from git ignore

--- src/store/actions/places.js
+++ src/store/actions/places.js
@@ -3,10 +3,7 @@ import {ADD_PLACE, DELETE_PLACE } from './actionTypes';
 
 export const addPlace = (placeName, location, image) => {
    return dispatch => {
-       const placeData = {
-           name: placeName,
-           location: location
-       };
+       
        fetch("https://us-central1-react-native-android-80076.cloudfunctions.net/storeImage", {
            method: "POST",
            body: JSON.stringify({
@@ -16,17 +13,21 @@ export const addPlace = (placeName, location, image) => {
        .catch(err => console.log(err))
        .then(res => res.json())
        .then(parsedRes => {
-           console.log(parsedRes);
-       });
-    //    fetch("https://react-native-android-80076.firebaseio.com/places.json", {
-    //        method: "POST",
-    //        body: JSON.stringify(placeData)
-    //    })
-    //    .catch(err => console.log(err))
-    //    .then(res => res.json())
-    //    .then(parsedRes => {
-    //        console.log(parsedRes)
-    //    })
+            const placeData = {
+                name: placeName,
+                location: location,
+                image: parsedRes.imageUrl
+            };
+           return fetch("https://react-native-android-80076.firebaseio.com/places.json", {
+            method: "POST",
+            body: JSON.stringify(placeData)
+        })
+       })
+       .catch(err => console.log(err))
+       .then(res => res.json())
+       .then(parsedRes => {
+           console.log(parsedRes)
+       })
    }
 }
 
