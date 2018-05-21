a67c1300979b3123bd9a8d8b937f67068b6870ac
Brane Opacic <brane.opacic@brightmarbles.io>
2018-05-21 15:23:11 +0100

    Added logic for sending image to firebase storage

--- src/store/actions/places.js
+++ src/store/actions/places.js
@@ -2,33 +2,32 @@ import {ADD_PLACE, DELETE_PLACE } from './actionTypes';
 
 
 export const addPlace = (placeName, location, image) => {
-    return dispatch => {
-        const placeData = {
-            name: placeName,
-            location: location
-        }
-
-        fetch("https://console.firebase.google.com/project/react-native-android-80076/overview", {
-            method: "POST",
-            body: JSON.stringify({
-                image: image.base64
-            })
-        })
-        .catch(err => console.log(err))
-        .then(res => res.json())
-        .then(parsedRes => {
-            console.log(parsedRes)
-        })
-        // fetch("https://react-native-android-80076.firebaseio.com/places.json", {
-        //     method: "POST",
-        //     body: JSON.stringify(placeData)
-        // })
-        // .catch(err => console.log(err))
-        // .then(res => res.json())
-        // .then(parsedRes => {
-        //     console.log(parsedRes)
-        // })
-    }
+   return dispatch => {
+       const placeData = {
+           name: placeName,
+           location: location
+       };
+       fetch("https://us-central1-react-native-android-80076.cloudfunctions.net/storeImage", {
+           method: "POST",
+           body: JSON.stringify({
+               image: image.base64
+           })
+       })
+       .catch(err => console.log(err))
+       .then(res => res.json())
+       .then(parsedRes => {
+           console.log(parsedRes);
+       });
+    //    fetch("https://react-native-android-80076.firebaseio.com/places.json", {
+    //        method: "POST",
+    //        body: JSON.stringify(placeData)
+    //    })
+    //    .catch(err => console.log(err))
+    //    .then(res => res.json())
+    //    .then(parsedRes => {
+    //        console.log(parsedRes)
+    //    })
+   }
 }
 
 export const deletePlace = (key) => {
