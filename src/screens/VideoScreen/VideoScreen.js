5e048928d3502de3356945bb1ba215129e39f215
Brane Opacic <brane.opacic@brightmarbles.io>
2018-06-08 14:04:33 +0200

    Implemented the logic for video playing on separate VideoScreen

--- src/screens/VideoScreen/VideoScreen.js
+++ src/screens/VideoScreen/VideoScreen.js
@@ -1,16 +1,36 @@
 import React, { Component } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import Video from 'react-native-video';
+import LightVideo from '../../assets/background.mp4';
 
 
 class VideoScreen extends Component {
     render(){
         return(
-            <View>
-                <Text>Tryout</Text>
+            <View style={styles.container}>
+                <Video
+                source={LightVideo}
+                resizeMode="cover"
+                style={StyleSheet.absoluteFill}
+                shouldPlay={true}
+                paused={false}
+                repeat={true}
+                rate={1.0}
+                volume={1.0}
+                muted={false}
+                />
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        width: "100%",
+        height: "100%",
+        backgroundColor: "red"
+    }
+})
 
 export default VideoScreen;
