b94f0e5454fa3cb57147d739c38caf30a4619435
Brane Opacic <brane.opacic@brightmarbles.io>
2018-06-11 14:05:34 +0200

    Updated readme.md with info relevant to project

--- index.js
+++ index.js
@@ -1,17 +1,6 @@
 import React from 'react';
 import { AppRegistry } from 'react-native';
-import { Provider } from 'react-redux';
+
 import App from './App';
-import configureStore from './src/store/configureStore';
 
-
-const store = configureStore()
-
-
-const RNRedux = () => (
-    <Provider store={store}>
-        <App />
-    </Provider>
-)
-
-AppRegistry.registerComponent('reactnativeapp', () => RNRedux);
+AppRegistry.registerComponent('reactnativeapp', () => <App />);
